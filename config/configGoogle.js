50dc837ee45fb6057fbcf182562d67027265e854
duongdatnguyen <55284972+duongdatnguyen@users.noreply.github.com>
2021-11-21 01:13:54 +0700

    Add call back url

--- config/configGoogle.js
+++ config/configGoogle.js
@@ -1,5 +1,9 @@
 module.exports={
     lientID: "907790633444-0fnqh5mpf12k1jfes1pal08gv51vhnsh.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://tlcnfashionwebsite.herokuapp.com/auth/google/callback"
 }
+/**http://localhost:3000/auth/google/callback
+ * 
+ * https://tlcnfashionwebsite.herokuapp.com/
+ */
